ae91ff012afe29f1adbb5c2eedc15fda846aa3f4
Jerry <Jerry_sunzhen@163.com>
2021-07-23 10:04:17 +0800

    init admin project

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+// 配置文件
+
+module.exports = {
+
+  title: 'Vue2_Admin_Project_PC',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
